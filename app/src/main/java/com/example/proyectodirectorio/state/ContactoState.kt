8af13a6fab32da292ea25502e248b52fb66cde42
Christian Venegas <venegaschristian014@gmail.com>
2025-06-01 09:21:10 -0600

    Se creo el estado del modelo

---
+++ app/src/main/java/com/example/proyectodirectorio/state/ContactoState.kt
@@ -0,0 +1,9 @@
+package com.example.proyectodirectorio.state
+
+data class ContactoState(
+    val nombre: String = "",
+    val apellidoPaterno: String = "",
+    val apellidoMaterno: String = "",
+    val correo: String = "",
+    val numero: String = "",
+)
